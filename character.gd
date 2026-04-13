7209d8d55b58992e3f27a6b04bdc281aa2811d49
FunnyParadox <76780477+FunnyParadox@users.noreply.github.com>
2026-04-13 12:41:46 +0200

    Add jump charge mechanic and update character scene
    Introduce a charged jump: add JUMP_CHARGE_SPEED and jump_force, change jump handling to accumulate charge while Jump is held and apply impulse on release using a gravity vector. Adjust gravity application to reuse a rotated gravity vector and increase base JUMP_VELOCITY. Minor constant formatting change (DECELERATION) and commented-out legacy directional input. Update character.tscn to a CharacterBody2D scene (attach character.gd script) and add CollisionShape2D/Polygon2D/subresource shape. Update debug.gd to display jump force and reformat debug lines.

--- character.gd
+++ character.gd
@@ -1,40 +1,48 @@
 extends CharacterBody2D
 
 const ACCELERATION = 160.0
-const DECELERATION = 0.395
+const DECELERATION = 1./3.
 const MAX_SPEED = 372.0
 
 const ACCELERATION_LIMIT = 0.60
 const GRAVITY_FORCE = 1500.0
 
-const JUMP_VELOCITY = 500.0
+const JUMP_VELOCITY = 620.0
+const JUMP_CHARGE_SPEED = 0.74 # in seconds
 const COYOTE_JUMP = sqrt(2)/10
 
 var air_time := 0.0
+var jump_force := 0.0
 var has_jumped := false
 var last_acceleration := 0.0
 var gravity_angle := 0.0
 var direction := 1 # -1: left, 1: right
 
 func _physics_process(delta: float) -> void:
+	var gravity := Vector2(0, 1).rotated(gravity_angle)
+
 	# Add the gravity.
 	if not is_on_floor():
 		air_time += delta
-		velocity += Vector2(0, 1).rotated(gravity_angle) * GRAVITY_FORCE * delta
+		velocity += gravity * GRAVITY_FORCE * delta
 	else:
 		air_time = 0
 		has_jumped = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and air_time < COYOTE_JUMP and not has_jumped:
-		velocity.y = -JUMP_VELOCITY
+	if Input.is_action_just_released("Jump") and air_time < COYOTE_JUMP and not has_jumped:
+		velocity -= gravity * (velocity.dot(gravity) + JUMP_VELOCITY) * jump_force
 		has_jumped = true
+	if Input.is_action_pressed("Jump"):
+		jump_force = min(jump_force + delta / JUMP_CHARGE_SPEED, 1.0)
+	else:
+		jump_force = 0
 
 	# Get the input direction and handle the movement/deceleration.
-	if Input.is_action_just_pressed("Left"):
-		direction = -1
-	if Input.is_action_just_pressed("Right"):
-		direction = not Input.is_action_just_pressed("Left")
+##	if Input.is_action_just_pressed("Left"):
+##		direction = -1
+##	if Input.is_action_just_pressed("Right"):
+##		direction = not Input.is_action_just_pressed("Left")
 
 	if air_time < COYOTE_JUMP and Input.is_action_just_pressed("Accelerate"):
 		velocity.x += direction * ACCELERATION * pow(min(last_acceleration / ACCELERATION_LIMIT, 1.0), 2.0)
--- debug.gd
+++ debug.gd
@@ -13,10 +13,12 @@ func _draw() -> void:
 	var font = ThemeDB.fallback_font
 	var font_size = 16
 	var text_position = Vector2(font_size, font_size)
-	draw_string(font, text_position, " Position: " + str("%0.3f / %0.3f" % [character.position.x, character.position.y]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
+	draw_string(font, text_position, "  Position: " + str("%0.3f / %0.3f" % [character.position.x, character.position.y]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
 	text_position.y += font_size * 1.5
-	draw_string(font, text_position, " Velocity: " + str("%0.3f / %0.3f" % [character.velocity.x, character.velocity.y]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
+	draw_string(font, text_position, "  Velocity: " + str("%0.3f / %0.3f" % [character.velocity.x, character.velocity.y]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
 	text_position.y += font_size * 1.5
-	draw_string(font, text_position, "  Airtime: " + str("%0.3f / %0.3f (%s)" % [character.air_time, character.COYOTE_JUMP, "O" if (character.air_time < character.COYOTE_JUMP) else "X"]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
+	draw_string(font, text_position, "   Airtime: " + str("%0.3f / %0.3f (%s)" % [character.air_time, character.COYOTE_JUMP, "O" if (character.air_time < character.COYOTE_JUMP) else "X"]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
 	text_position.y += font_size * 1.5
-	draw_string(font, text_position, "Last Acc.: " + str("%0.3f / %0.3f (%03d%%)" % [character.last_acceleration, character.ACCELERATION_LIMIT, int(min(character.last_acceleration / character.ACCELERATION_LIMIT, 1.0) * 100.0)]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
+	draw_string(font, text_position, "Jump Force: " + str("%03d%%" % [int(character.jump_force * 100.0)]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
+	text_position.y += font_size * 1.5
+	draw_string(font, text_position, " Last Acc.: " + str("%0.3f / %0.3f (%03d%%)" % [character.last_acceleration, character.ACCELERATION_LIMIT, int(min(character.last_acceleration / character.ACCELERATION_LIMIT, 1.0) * 100.0)]), HORIZONTAL_ALIGNMENT_LEFT, -1, font_size)
